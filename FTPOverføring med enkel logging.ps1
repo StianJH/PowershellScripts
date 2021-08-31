49fc2ad7c07686f49bb765bd020b220c2e0a77ca
StianJH <89838331+StianJH@users.noreply.github.com>
2021-08-31 11:07:51 +0200

    FTP_Filoverføring_ps1
    Enkel filoverføring med logging basert på kopiering/FTP.

---
+++ FTPOverføring med enkel logging.ps1
@@ -0,0 +1,23 @@
+﻿#Variabler som definerer plasseringene. Husk å ha med \ på slutten for å ta inne i selve mappen og ikke selve mappen.
+$Source = "\\SERVER\MAPPE\MAPPE\"
+$Destination = "\\SERVER\D$\MAPPE\"
+
+#Variabler som definerer tidspunktet det blir kjørt og genererer log fil.
+$Timestamp = Get-Date -uformat "%d/%m/%y %R"
+$Logfile = "D:\Scripts\Logs\" + "LOGNAVN" + ".log"
+
+#Variabel som henter filer som er under 10 timer gamle.
+$Files = Get-ChildItem $Source | where-object {$_.LastWriteTime -gt (get-date).AddHours(-10)}
+
+#Hvis variabelen Files er høyere enn 1 så utfør:
+If($Files.count -gt 1)
+{
+$Files | Copy-Item -Destination $Destination
+Add-Content $Logfile "$($Timestamp): Info: Files moved successfully"
+}
+
+#Hvis ikke over er TRUE så utfør:
+Else{
+$Timestamp = Get-Date -uformat "%d/%m/%y %R"
+Add-Content $Logfile "$($timestamp): Error: No new files in folder"
+}
